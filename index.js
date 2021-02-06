6396cde89eff2b8d87385451a21d4b1a665cf3dd
Hyper Water Dog <omegaflakes@gmail.com>
2021-02-05 20:15:57 -0800

    added win checking

--- index.js
+++ index.js
@@ -35,10 +35,29 @@ let gameboard  = (function (){
         }
     };
 
+    let wonGame = function(){
+        let ans = false;
+        gameboard.forEach(row=>  //check each row for a win
+            {if(row.every(ele=>ele==row[0]&&ele!='')){
+                ans = row[0];
+            }}
+            );
+        
+        for (i=0;i<3;i++){
+            let column = [...gameboard[0][i],gameboard[1][i],gameboard[2][i]];
+            if(column.every(ele=>ele==column[0]&&ele!='')){
+                ans = column[0];
+            };
+        };
 
+        if((gameboard[0][0]==gameboard[1][1]&&gameboard[1][1]==gameboard[2][2]&&gameboard[1][1]!='')||(gameboard[0][2]==gameboard[1][1]&&gameboard[1][1]==gameboard[2][0]&&gameboard[1][1]!='')){
+            ans = gameboard[1][1];
+        }
+        return ans
+    };
 
     return {
-        getBoard,piecesPlayed,playPiece
+        getBoard,playPiece,wonGame
     }
 })();
 
@@ -46,8 +65,8 @@ let displayController = (function(){
     let board = gameboard.getBoard();
     let grid = document.querySelector('#grid');
 
-    let intializeBoard = function(){
-        clearBoard()
+    let intializeBoard = function(){ //function that updates the display to match the game board
+        clearBoard();
         for (i=0;i<3;i++){
             for(j=0;j<3;j++){
                 let temp = document.createElement('div');
@@ -76,9 +95,18 @@ let displayController = (function(){
     let userClick = function(e){  //function that adds a piece when a user clicks
         let [x,comma,y] = e.target.getAttribute('data-key');
         gameboard.playPiece(x,y);
-        console.table(gameboard.getBoard())
-        displayController.intializeBoard()
-    }
+        console.table(gameboard.getBoard());
+        displayController.intializeBoard();
+        userWon();
+    };
+
+    let userWon = function(){
+        if(gameboard.wonGame()!=false){
+            console.log(gameboard.wonGame())
+            console.log(gameboard.getBoard())
+            alert('won')
+        }
+    };
 
     return {
         intializeBoard
@@ -95,8 +123,13 @@ bill.increaseScore();
 console.log(bill.getScore());
 console.log(bill.getUserName());
 
-gameboard.playPiece(1,2)
-console.log(gameboard.piecesPlayed())
+// gameboard.playPiece(0,0)
+// gameboard.playPiece(1,2)
+// gameboard.playPiece(0,1)
+// gameboard.playPiece(2,2)
+// gameboard.playPiece(0,2)
+
+console.log(gameboard.wonGame())
 console.log(gameboard.getBoard())
 
 //main code
